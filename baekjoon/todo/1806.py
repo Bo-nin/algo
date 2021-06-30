c7026758d2a7e7d2f998272e8654051056ca1f6b
sloppy-cat <js3891129@gmail.com>
2021-07-01 00:02:44 +0900

    init 1806

--- baekjoon/todo/1806.py
+++ baekjoon/todo/1806.py
@@ -4,16 +4,21 @@
 
 n, s = list(map(int, input().split()))
 numList = list(map(int, input().split()))
-
+pSum = 0
 l, r = 0, 1
-_min = 100000
+_min = 100001
 while(1):
-  _sum = sum(numList[l:r])
-  if _sum >= s:
-    _min = min(_min, r-l)
+  if pSum >= s:
+    if _min > r-l:
+      _min = r-l
+    pSum -= numList[l]
     l += 1
   elif r == n-1:
     break
   else:
     r += 1
-print(_min)
+    pSum += numList[r]
+if _min == 100001:
+  print(0)
+else:
+  print(_min)
